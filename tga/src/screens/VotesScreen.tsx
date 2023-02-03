67c0dc576a2f9a10f59ca18e8cc0404cbd04c7bc
Giovanna <giovannamsantanaa@gmail.com>
2023-02-03 14:03:16 -0300

    Adicionando os arquivos da da tela principal

---
+++ tga/src/screens/VotesScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+// "Lib com as partes dos componentes"
+import { View, Text } from "react-native";
+
+export function VotesScreen() {
+    return (
+        <View>
+
+        </View>
+    )
+}
