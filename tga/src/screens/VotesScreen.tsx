20b6b0f142ca8dd433b4d838047d731e0b75e136
Giovanna <giovannamsantanaa@gmail.com>
2023-02-07 11:42:09 -0300

    Criando a estrutura inicial das telas

--- tga/src/screens/VotesScreen.tsx
+++ tga/src/screens/VotesScreen.tsx
@@ -1,11 +1,23 @@
+import { setStatusBarBackgroundColor } from "expo-status-bar";
 import React from "react";
-// "Lib com as partes dos componentes"
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export function VotesScreen() {
     return (
-        <View>
-
+        // Para aplicar o style na tela
+        <View style={styles.container}>
+            <Text>Testando</Text>
         </View>
     )
 }
+
+// Estilizando a as telas
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        color: '#fff',
+        backgroundColor: '#191919',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
